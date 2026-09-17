eba11b71f01444db9e584721b8d45414a999133b
Djaeger1 <79890209+Djaeger1@users.noreply.github.com>
2026-09-17 19:54:38 +0700

    Keep Xposed stubs JVM-only

--- facebook-ad-shield-526/xposed-stubs/src/main/java/de/robv/android/xposed/callbacks/XC_LoadPackage.java
+++ facebook-ad-shield-526/xposed-stubs/src/main/java/de/robv/android/xposed/callbacks/XC_LoadPackage.java
@@ -1,12 +1,10 @@
 package de.robv.android.xposed.callbacks;
 
-import android.content.pm.ApplicationInfo;
-
 public abstract class XC_LoadPackage {
     public static class LoadPackageParam {
         public String packageName;
         public String processName;
         public ClassLoader classLoader;
-        public ApplicationInfo appInfo;
+        public Object appInfo;
     }
 }
